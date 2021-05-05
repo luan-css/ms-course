1437bd7593d41d1abef2cbd87f5df599c0f295fc
luan-css <sluan298@gmail.com>
2021-05-05 20:49:37 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/dev/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/dev/hrpayroll/resources/PaymentResource.java
@@ -3,6 +3,7 @@ package com.dev.hrpayroll.resources;
 import com.dev.hrpayroll.entities.Payment;
 import com.dev.hrpayroll.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentResource {
+
     @Autowired
     private PaymentService service;
 
