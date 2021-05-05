79eb88a7e402880a8226184b3c417a2b8b9231ba
luan-css <sluan298@gmail.com>
2021-05-05 18:50:59 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/dev/hrpayroll/services/PaymentService.java
@@ -0,0 +1,11 @@
+package com.dev.hrpayroll.services;
+
+import com.dev.hrpayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+    public Payment getPayment(long workerId, int days){
+        return new Payment("Luan", 100.0, days);
+    }
+}
